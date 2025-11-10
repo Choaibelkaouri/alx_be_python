d37105a6096704d90ea25fdef1aff94bb9488aff
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-11-10 19:10:16 +0100

    Add files via upload

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    print(f"{num} * {i} = {num * i}")
+
+for i in range (2,5):
+    print(f"{num}+{i}={num+i}")
