1590cc6be35f6a673b65dba5f85e3adb6e7e33e1
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-11-17 19:38:11 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,31 @@
 
+def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Perform a basic arithmetic operation on two numbers.
+
+    :param num1: first number (float)
+    :param num2: second number (float)
+    :param operation: one of 'add', 'subtract', 'multiply', 'divide'
+    :return: result of the operation, or an error message
+    """
+
+    operation = operation.lower()
+
+    if operation == "add":
+        return num1 + num2
+
+    elif operation == "subtract":
+        return num1 - num2
+
+    elif operation == "multiply":
+        return num1 * num2
+
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1 / num2
+
+    else:
+        return "Error: Invalid operation"
+
+
